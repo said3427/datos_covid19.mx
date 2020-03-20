33be12e2072eb653accbe42b6f587db83e3409b3
said3427 <said3427@gmail.com>
2020-03-20 00:33:25 -0600

    19 marzo

--- get_data_mx.r
+++ get_data_mx.r
@@ -4,32 +4,23 @@ library("tabulizer")
 library("dplyr")
 library("lubridate")
 library("stringr")
-library("tidyRSS")
+library("XML")
+library("ggplot2")
 # Lectura de archivos oficiales
 
-## RSS feed
-gobMX<- xmlParse("http://fetchrss.com/rss/5e7304aa8a93f815318b45675e7303918a93f816258b4567.atom")
-gobMX<- xmlToDataFrame(gobMX)
-pdf_file<-gobMX$id[grepl(pattern = "casos_positivos",gobMX$id)]
 ## PDF actualizado
-#pdf_file <- "https://www.gob.mx/cms/uploads/attachment/file/542104/Tabla_casos_positivos_COVID-19_resultado_InDRE_2020.03.18.pdf"
+pdf_file <- "https://www.gob.mx/cms/uploads/attachment/file/542354/Tabla_casos_positivos_COVID-19_resultado_InDRE_2020.03.19.pdf"
 
 out <- extract_tables(pdf_file)
 ## Concatena las tablas de todas las hojas
 final <- do.call(rbind, out[-1])
 
 ## Remueve las primeras filas que corresponden al encabezado
-final<-data.frame(final)
+final<-data.frame(final,stringsAsFactors = F)
 
 colnames(final)<-c("Caso","Estado","Sexo","Edad","Inicio","Confirmación","Procedencia","Llegada")
 
-final$Reporte<-ymd(today())
-referencia<-data.table::fread("https://gist.githubusercontent.com/said3427/18f39eab460766a17ea4802607dd5522/raw")
-
-duprows <- final$Caso %in% referencia$Caso
-final<-rbind(referencia, final[!duprows,],use.names=F)
-
-
+final$Reporte<-ymd("2020-03-19")
 final$Inicio<-ymd(as.Date(final$Inicio,format="%d/%m/%y"))
 final$Llegada<-ymd(as.Date(final$Llegada,format="%d/%m/%y"))
 
@@ -38,6 +29,15 @@ final$Estado<-str_to_title(final$Estado)
 
 ## Cambiar la M por male y F por female
 final$Sexo<-str_replace_all(final$Sexo,c("M"="male","F"="female"))
+
+
+referencia<-data.table::fread("https://gist.githubusercontent.com/said3427/18f39eab460766a17ea4802607dd5522/raw/34f39e8fa84606c0a8c3c52cd50df153ea9d6682/covid-19_Mexico.csv",
+                              colClasses = c("integer","character","character","integer","Date","character","character","Date","Date"))
+
+duprows <- final$Caso %in% referencia$Caso
+final<-rbind(referencia, final[!duprows,],use.names=F)
+final$Caso<-as.numeric(final$Caso)
+final$Edad<-as.numeric(final$Edad)
 
 ## Escribe la base mexicana
 write.csv(final,file="covid19_cases.csv",quote=F,row.names = F)
